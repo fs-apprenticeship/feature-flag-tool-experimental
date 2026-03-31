b1e71e146a9520414edce179e1181d4e008cc0c5
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2026-03-31 19:31:36 -0400

    Create simple react form for creating flags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+
   return (
+
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
@@ -58,6 +64,9 @@ export default function Home() {
           >
             Documentation
           </a>
+         <button onClick={()=> router.push("/flags/new")} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">
+           Create a flag
+         </button>
         </div>
       </main>
     </div>
