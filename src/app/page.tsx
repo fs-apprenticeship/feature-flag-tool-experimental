ae2b51f3f9855a6319b73bc62d0bc9e24a4ef67e
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2026-04-14 13:13:20 -0400

    Finalize home page, update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
   return (
 
-    <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100 font-sans dark:bg-black gap-30">
+  <main className="flex flex-col min-h-screen items-center justify-center bg-gray-100 font-sans dark:bg-black gap-30 border-1 border-black"> 
       <div className="flex flex-col items-center justify-center gap-2">
           <h1 className="flex items-center mb-2 text-4xl font-bold text-heading md:text-5xl lg:text-6xl gap-4">
             <div className="bg-[#00B3E6] flex h-16 w-16 items-center justify-center rounded-xl" > 
@@ -43,22 +43,14 @@ export default function Home() {
           <h5 className="mb-4 text-lg font-normal text-body lg:text-xl">
             Toggle features across every environment without redeploying. 
           </h5>
-          <SignUpButton> 
-            <Button size="lg" className="rounded-4xl py-6 px-10"> Sign up </Button>
+          <SignUpButton > 
+            <Button onClick={()=> router.push("/protected")} size="lg" className="rounded-4xl py-6 px-10"> Sign up </Button>
           </SignUpButton>
         </div>
       
-        {/* <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-         <button onClick={()=> router.push("org/flatiron-school/projects/fis-demo/flags/new")} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">
-           Create a flag
-         </button>
-         <button onClick={()=> router.push("org/flatiron-school/projects/fis-demo/flags")} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">
-           See all flags
-         </button> */}
-
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 w-full max-w-5xl">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 max-w-5xl">
         {features.map((f) => (
-          <Card key={f.title} className="p-10">
+          <Card key={f.title} className="p-8">
             <CardContent className="p-0 flex flex-col gap-2">
               <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-[#e0f7fd] text-[#00B3E6]">
                 {f.icon}
@@ -69,7 +61,6 @@ export default function Home() {
           </Card>
         ))}
       </div>
-        
-    </div>
+    </main>
   );
 }
---
+++ src/components/shared/Sidebar.tsx
@@ -0,0 +1,42 @@
+// import {
+//   Sidebar,
+//   SidebarMenu,
+//   SidebarMenuItem,
+//   SidebarContent,
+//   SidebarMenuButton,
+//   SidebarFooter,
+//   SidebarGroup,
+//   SidebarHeader,
+//   SidebarTrigger
+// } from "@/components/ui/sidebar"
+
+// export default function AppSidebar() {
+//   return (
+//    <Sidebar>
+//     <SidebarHeader>
+//     <SidebarMenu>
+//       <SidebarMenuItem>
+//         <SidebarMenuButton>
+//             Workspace
+//         </SidebarMenuButton>
+//         <SidebarTrigger/>
+
+//         {/* <DropdownMenu>
+//           <DropdownMenuTrigger asChild>
+//             <SidebarMenuButton>
+//               Select Workspace
+//               <ChevronDown className="ml-auto" />
+//             </SidebarMenuButton>
+//           </DropdownMenuTrigger>
+//           <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+//             <DropdownMenuItem>
+//               <span>Acme Inc</span>
+//             </DropdownMenuItem>
+//           </DropdownMenuContent>
+//         </DropdownMenu> */}
+//       </SidebarMenuItem>
+//     </SidebarMenu>
+//   </SidebarHeader>
+// </Sidebar>
+//   )
+// }
