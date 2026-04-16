fe2be1c6c2679716927ac23dec6424522eecdd22
Damian Stevenson <118267371+reaperbill@users.noreply.github.com>
2026-04-16 12:04:25 -0500

    Adding auth-sync
    This was originally put in the default page. Since Signin and Signup were moved here, moving the components here

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar() {
         </div>
         <div className="flex gap-4 items-center"> 
         <Show when="signed-out">
-            <SignInButton />
-            <SignUpButton>
+            <SignInButton forceRedirectUrl={"/auth-sync"}/>
+            <SignUpButton forceRedirectUrl={"/auth-sync"}>
             <button className="bg-[#00B3E6] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                 Sign Up
             </button>
