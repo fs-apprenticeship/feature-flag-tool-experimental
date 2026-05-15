225a6bf43f4f1ae66756b23ad9f8cf0890c5e621
Jae Jeong <jeongjj29@gmail.com>
2026-05-15 16:26:46 -0400

    fix(orgs): resolve organization build type errors

--- src/features/orgs/models/Organization.ts
+++ src/features/orgs/models/Organization.ts
@@ -1,30 +1,14 @@
 export interface Organization {
   id: string;
   name: string;
+  key: string;
+  slug: string;
 }
 
-export interface OrgRow {
-    id: string;
-    name: string;
-    key: string;
-    slug: string;
-}
-
-export type CreateOrganization = Omit<Organization, 'id'>;
+export type CreateOrganization = {
+  name: string;
+};
 
 export interface CreateOrgOptions {
   data: CreateOrganization;
-}
-
-export class Organization {
-    id: string;
-    name: string;
-    key: string
-    slug: string;
-    constructor(row: OrgRow) {
-        this.id = row.id;
-        this.name = row.name;
-        this.key = row.key;
-        this.slug = row.slug;
-  }
 }
